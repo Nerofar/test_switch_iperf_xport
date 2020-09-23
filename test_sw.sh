ee1463afebaec1e4435b79288e443f8123c249b0
Nerofar <nerofar@gmail.com>
2020-09-23 11:15:18 +0300

    Update test_sw.sh

--- test_sw.sh
+++ test_sw.sh
@@ -6,6 +6,18 @@ BACKTITLE="Test SW"
 infe=eth1
 ports=$(dialog --stdout --title "Ввод данных" --clear --inputbox " \nК-во проверяемых портов:" 16 51)
 let ports=$ports+1200-1
+for x in $(seq 1200 $ports)
+do
+    echo "DEVICE=vlan${x}" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+    echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+    echo "BOOTPROTO=none" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+    echo "IPADDR=192.168.${x}.1" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+    echo "NETMASK=255.255.255.0" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+    echo "NETWORK=10.0.${x}.0" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+    echo "BROADCAST=10.0.${x}.255" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+    echo "PHYSDEV=$infe" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+    echo "VID=${x}" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
+done
 
 TITLE="any"
 MENU="Исходящий свич:"
