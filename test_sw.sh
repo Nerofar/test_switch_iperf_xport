1432ec3e21d9fc7031654ab731f0f5721b856491
Nerofar <nerofar@gmail.com>
2021-01-28 14:56:04 +0200

    Update test_sw.sh

--- test_sw.sh
+++ test_sw.sh
@@ -43,10 +43,51 @@ rm -fr /tmp/ports
 let ports=$ports+1200-1
 for x in $(seq 1200 $ports)
 do
-    let y=${x}-1200
-    nmcli con add type vlan con-name ${infe}.${x} id ${x} dev ${infe} ip4 10.0.${y}.11/24
-    nmcli con up ${infe}.${x}
+let y=${x}-1200
+#modprobe 8021q
+#создание влан чёт вход, нечёт выход
+
+  evenCheck=$(expr ${x} % 2)
+  if [ $evenCheck = 0 ] ;
+    then
+    #вланы входящие
+#    vconfig add ${infe} ${x}
+#    nmcli con add type vlan con-name ${infe}.${x} id ${x} dev ${infe} ip4 10.0.${y}.12/24
+#    nmcli con up ${infe}.${x}
+    #неймспейс
+    ##
+    ip netns add iperf-server${x}
+    ip link set ${infe}.${x} netns iperf-server${x}
+    ip netns exec iperf-server${x} ip addr add dev ${infe}.${x} 10.0.${y}.11/24
+    ip netns exec iperf-server${x} ip link set dev ${infe}.${x} up
+
+#    ip netns exec iperf-server${x} ip addr add dev ${infe}.${x} 10.0.${y}.11/24
+#    ip netns exec iperf-server${x} ip link set dev ${infe}.${x} up
+    ##
+    $DIALOG --title "Testing switch" --msgbox "test 1 переменная x ${x}и y ${y}" 10 40
+  else
+    #вланы исходящие
+    let y=${y}-1
+#    vconfig add ${outfe} ${x}
+#    nmcli con add type vlan con-name ${outfe}.${x} id ${x} dev ${outfe} ip4 10.0.${y}.12/24
+#    nmcli con up ${outfe}.${x}
+    #неймспейс
+    ip netns add iperf-client${x}
+    ip link set ${outfe}.${x} netns iperf-client${x}
+    ip netns exec iperf-client${x} ip addr add dev ${outfe}.${x} 10.0.${y}.12/24
+    ip netns exec iperf-client${x} ip link set dev ${outfe}.${x} up
+
+#    ip netns exec iperf-client${x} ip addr add dev ${outfe}.${x} 10.0.${y}.12/24
+#    ip netns exec iperf-client${x} ip link set dev ${outfe}.${x} up
+    ##
+    ##
+    $DIALOG --title "Testing switch" --msgbox "test 1 переменная x ${x}и y ${y}" 10 40
+  fi
+
+#
 done
+
+
 $DIALOG --title "Testing switch" --msgbox "etap 5" 10 40
 TITLE="any"
 MENU="Исходящий свич:"
@@ -60,19 +101,9 @@ OPTIONS=(1 "D-link"
 clear
 case $CHOICE in
       1)
-      echo "Выбран D-link"
-        for x in $(seq 1200 $ports)
-        do
-            echo "DEVICE=vlan${x}" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-            echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-            echo "BOOTPROTO=none" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-            echo "IPADDR=192.168.${x}.1" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-            echo "NETMASK=255.255.255.0" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-            echo "NETWORK=10.0.${x}.0" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-            echo "BROADCAST=10.0.${x}.255" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-            echo "PHYSDEV=$infe" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-            echo "VID=${x}" >> /etc/sysconfig/network-scripts/ifcfg-vlan${x}
-        done
+      ip netns exec iperf-server1200 iperf3 -s
+      ip netns exec iperf-client1201 iperf3 -c 10.0.0.11 -P 10 -t 300
+
       ;;
       2)
       echo "Выбран с chroot"
@@ -84,4 +115,7 @@ case $CHOICE in
 esac
 
 #clear vlan
-##rm -fr /etc/sysconfig/network-scripts/ifcfg-vlan*
+##rm -fr /etc/sysconfig/network-scripts/enp1s0f0.12*
+##rm -fr /etc/sysconfig/network-scripts/enp1s0f1.12*
+##delete all namespace
+##ip -all netns delete
