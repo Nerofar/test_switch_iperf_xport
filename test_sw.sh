5ee55341413c856fd72b0b07e240f147c984b95f
Nerofar <nerofar@gmail.com>
2021-02-04 13:29:47 +0200

    Update test_sw.sh

--- test_sw.sh
+++ test_sw.sh
@@ -121,12 +121,12 @@ case $CHOICE in
           then
             ip netns exec iperf-server${z} iperf3 -s --logfile s${z}.log &
           ##
-          $DIALOG --title "Testing switch" --msgbox "test 1 переменная iperf-s:\n iperf-server${z} и a 10.0.${a}.11 \n iperf-server${x} 10.0.${y}.11/24" 10 40
+          #$DIALOG --title "Testing switch" --msgbox "test 1 переменная iperf-s:\n iperf-server${z} и a 10.0.${a}.11 \n iperf-server${x} 10.0.${y}.11/24" 10 40
           else
             let a=${a}-1
-            ip netns exec iperf-client${z} iperf3 -c 10.0.${a}.11 -t ${time} &
+            ip netns exec iperf-client${z} iperf3 -c 10.0.${a}.11 -t ${time} & 2> /dev/null
           ##
-          $DIALOG --title "Testing switch" --msgbox "test 2 переменная iperf-c:\n iperf-client${z} и a 10.0.${a}.12 \n iperf-client${x} 10.0.${y}.12/24" 10 40
+          #$DIALOG --title "Testing switch" --msgbox "test 2 переменная iperf-c:\n iperf-client${z} и a 10.0.${a}.12 \n iperf-client${x} 10.0.${y}.12/24" 10 40
         fi
 
       #
